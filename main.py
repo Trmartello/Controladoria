6b2ba24f4627b115e6a42883858277ed586c9a56
Tober Martello <90587069+Trmartello@users.noreply.github.com>
2023-09-21 20:25:10 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import time 
+import base64
+
+st.set_page_config(
+    page_title='Controller', 
+    page_icon='https://pittol.vteximg.com.br/arquivos/favicon.png?v=637013313767830000',
+    layout='wide',
+    initial_sidebar_state='expanded')
+
+    # st.sidebar.markdown("Here is some text")
+
+st.header('Pagina inicial')
